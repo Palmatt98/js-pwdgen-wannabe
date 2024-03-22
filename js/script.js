5231a4b781b740e6a2102ba5c427449bb64b390a
Palmatt98 <palmierimatteo98@gmail.com>
2024-03-22 17:27:56 +0100

    add: first exercise on javascript

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//età dell'utente
+let userName = prompt("ciao utente come qual è il tuo nome")
+console.log(userName, typeof userName);
+
+let userSurname = prompt("cognome")
+console.log(userSurname);
+alert(`ciao ${userName + userSurname}`)
+
+let userAge = prompt("quanti hanni hai?")
+console.log(userAge, typeof userAge); //srtring
+
+userAge = parseInt(userAge) //adesso si è trasmofrmato in NUMBER
+console.log(userAge, typeof userAge)
+
+const currentYear = 2024; //number
+//sottrazione dell'età corrente meno l'età dell'user da vita all'anno di nascita
+let birthAge = currentYear - userAge;
+console.log(birthAge);
+
+favoriteColor = prompt("qual è il tuo colore preferito")
+alert(`complimenti, molto bello  ${favoriteCOlor}`)
+
+document.getElementById ("result")
+.innerHTML = userName+userColor+userAge;
+
